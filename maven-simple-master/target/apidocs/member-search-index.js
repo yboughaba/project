17573d04bde256560c31383a1e0c3ed0ca9b1f90
yboughaba <52927487+yboughaba@users.noreply.github.com>
2021-02-03 15:47:05 +0100

    Add files via upload

---
+++ maven-simple-master/target/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.github.jitpack","c":"App","l":"App()","url":"%3Cinit%3E()"},{"p":"com.github.jitpack","c":"App","l":"greet(String)","url":"greet(java.lang.String)"},{"p":"com.github.jitpack","c":"App","l":"main(String[])","url":"main(java.lang.String[])"}]
